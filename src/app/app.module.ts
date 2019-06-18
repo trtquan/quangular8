c0af664343f33875b3435ed6118293f9d629a334
Tran <qtran31@dxc.com>
2019-06-18 10:31:19 +0700

    form: init substance, nav-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { SubstanceSecComponent } from './components/substance-sec/substance-sec.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    SubstanceSecComponent
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
---
+++ src/app/components/substance-sec/substance-sec.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { SubtanceSecFormService } from './substance-sec.service';
+
+@Component({
+  selector: 'substance-sec',
+  templateUrl: './substance-sec.component.html',
+  styleUrls: ['./substance-sec.component.scss'],
+})
+export class SubstanceSecComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
